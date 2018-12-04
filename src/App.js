efb18dbe50e01b15494802f219f0e9503e43dd49
ptrkap <ptrkap@gmail.com>
2018-12-04 16:51:02 +0100

    Removed offline

--- src/App.js
+++ src/App.js
@@ -4,11 +4,9 @@ import Table from './table/Table';
 import './App.css';
 
 export default function(){
-  let offline = <span id="offlineConnetion"> - offline</span>;
-  let serviceAvailable = false; // todo it is hardcoded now
   return (
     <>
-      <h2 id="title">Cars{!serviceAvailable && offline}</h2>
+      <h2 id="title">Cars</h2>
       <Add />
       <br />
       <Table />
